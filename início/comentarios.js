9aa87f7696c7214b6e870fd1f715dc360b2d73d5
ErickLuan009 <luanerick958@gmail.com>
2025-04-17 22:23:25 -0300

    Inicio com sentencas, comentarios, blocos e dados

---
+++ início/comentarios.js
@@ -0,0 +1,6 @@
+//console.log("Passo #03");
+console.log("Passo #01");
+console.log("Passo #02");
+/* O Console serve
+para imprimir
+resultados da tela */
---
+++ início/desafio_bloco_setenca.js
@@ -0,0 +1,13 @@
+/* Desafio: Criar 2 topicos de código.
+O primeiro bloco deve imprimir na tela os números pares de 0 a 10.
+E o segundo bloco de código deve imprimir os números ímpares de 0 a 10. */
+
+{
+    console.log("- Números pares de 0 a 10");
+    console.log("Os números são:0 - 2 - 4 - 6 - 8 - 10");
+    
+}
+{
+    console.log("- Os números ímpares de 0 a 10 ");
+    console.log("Os números são:1 - 3 - 5 - 7- 9");
+}
