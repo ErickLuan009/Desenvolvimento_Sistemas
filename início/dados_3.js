2978694ef84314e9c20c522bc1e415a1c232deb8
ErickLuan009 <luanerick958@gmail.com>
2025-04-24 22:01:50 -0300

    Dados e tipos de dados

---
+++ início/dados_3.js
@@ -0,0 +1,14 @@
+// Tipos de dados
+
+let idade = 23;
+console.log(typeof 23);
+console.log(typeof idade);
+
+let salario = 1412.50;
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo);
+
+const nome = "Erick"
+console.log(typeof nome);
---
+++ início/desafio_dados.js
@@ -0,0 +1,13 @@
+/* DESAFIO: Crie um progama que leia as variaveis PI
+e raio para que seja possível calcular o valor da área de circunferência. (PI * Raio)
+O programa deve imprimir na tela o valor calculando, utilizando a concetinização de string e variável.*/
+
+
+//const PI = 3.14;
+const RAIO = 10;
+//const area = PI *RAIO * RAIO;
+
+console.log("A área da circunfêrencia é: " + area);
+
+ var area = Math.PI * RAIO * RAIO;
+console.log("A área da circunfêrencia é: " + area);
